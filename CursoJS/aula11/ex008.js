0de93ff91ed226edf9075e4e70cac5a707e02ea6
davidcaue12 <davidcaue8@gmail.com>
2023-05-14 01:20:28 -0300

    Adição da Pasta de estudos de javaScript no Repositório

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+
+if(vel >= 60.5){//condição simples,caso não tenha o else
+    console.log(`A velocidade do seu carro é ${vel}`)
+    console.log('voce ultrapassou a velocidade permitida')
+}
